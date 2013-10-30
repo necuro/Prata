602bd456d5675f86fa3812e905901c2cdb9a25bf
Anton Melnyk <an.melnik@gmail.com>
2013-10-30 23:06:05 +0200

    task 3 done

--- chapter4/task3/tast3.cpp
+++ chapter4/task3/tast3.cpp
@@ -5,14 +5,15 @@ int main()
 {
    const char delim[] = {", "};
    std::cout << "Enter your first name: ";
-   char fname[20];
-   getline(std::cin, fname);
+   const short SIZE = 20;
+   char fname[SIZE];
+   std::cin.getline(fname, SIZE);
    std::cout << "Enter your last name: ";
-   char lname[20];
-   getline(cin, fname);
+   char lname[SIZE];
+   std::cin.getline(lname, SIZE);
    strcat( lname, delim );
    strcat( lname, fname );
-   std::cout << "Here's the information in a single string: " << fname << std::endl;
+   std::cout << "Here's the information in a single string: " << lname << std::endl;
 
    return 0;
 }
