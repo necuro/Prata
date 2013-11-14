a984d7d26ac0f7d8d78fb4a453b8b51b26e55de2
Anton Melnyk <an.melnik@gmail.com>
2013-11-14 23:02:27 +0200

    task9 is done

--- chapter4/task9/task9.cpp
+++ chapter4/task9/task9.cpp
@@ -14,24 +14,25 @@ struct CandyBar
 
 int main()
 {
-   CandyBar* candy = new CandyBar [3] { {"Roshen", 560, 300},
-                                        {"Svitoch", 2341, 400},
-                                        {"AVK", 897, 500} };
- cout << candy[0].Name;
-/*
-   cout << "Name: " << candy[0]->Name << endl
-        << "Weight: " << candy[0]->Weight << endl
-        << "Caloric: " << candy[0]->Caloric << endl;
+   CandyBar * candy = new CandyBar [3] { {"Roshen", 560, 300},
+                                         {"Svitoch", 2341, 400},
+                                         {"AVK", 897, 500} };
 
-   cout << "Name: " << candy[1]->Name << endl
-        << "Weight: " << candy[1]->Weight << endl
-        << "Caloric: " << candy[1]->Caloric << endl;
+   CandyBar* candy1 = new CandyBar { "Mars", 983, 600 };
 
-   cout << "Name: " << candy[2]->Name << endl
-        << "Weight: " << candy[2]->Weight << endl
-        << "Caloric: " << candy[2]->Caloric << endl;
+   cout << "Name: " << candy[0].Name << endl
+        << "Weight: " << candy[0].Weight << endl
+        << "Caloric: " << candy[0].Caloric << endl;
+
+   cout << "Name: " << candy[1].Name << endl
+        << "Weight: " << candy[1].Weight << endl
+        << "Caloric: " << candy[1].Caloric << endl;
+
+   cout << "Name: " << candy[2].Name << endl
+        << "Weight: " << candy[2].Weight << endl
+        << "Caloric: " << candy[2].Caloric << endl;
 
    delete [] candy;
-*/
+
    return 0;
 }
