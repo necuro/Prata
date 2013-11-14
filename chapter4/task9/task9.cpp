a2e6c529b47280a8ca6f5ea7d6d11da9ddc3c8ed
Anton Melnyk <an.melnik@gmail.com>
2013-11-14 17:57:12 +0200

    task9 in progress

---
+++ chapter4/task9/task9.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::endl;
+
+struct CandyBar
+{
+   string Name;
+   double Weight;
+   int Caloric;
+};
+
+int main()
+{
+   CandyBar* candy = new CandyBar [3] { {"Roshen", 560, 300},
+                                        {"Svitoch", 2341, 400},
+                                        {"AVK", 897, 500} };
+ cout << candy[0].Name;
+/*
+   cout << "Name: " << candy[0]->Name << endl
+        << "Weight: " << candy[0]->Weight << endl
+        << "Caloric: " << candy[0]->Caloric << endl;
+
+   cout << "Name: " << candy[1]->Name << endl
+        << "Weight: " << candy[1]->Weight << endl
+        << "Caloric: " << candy[1]->Caloric << endl;
+
+   cout << "Name: " << candy[2]->Name << endl
+        << "Weight: " << candy[2]->Weight << endl
+        << "Caloric: " << candy[2]->Caloric << endl;
+
+   delete [] candy;
+*/
+   return 0;
+}
