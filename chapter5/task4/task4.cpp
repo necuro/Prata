3f46067bade212016f20b6dd46b1f66e54969001
Anton Melnyk <an.melnik@gmail.com>
2013-11-18 17:35:52 +0200

    task4 in progress

---
+++ chapter5/task4/task4.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std:cin;
+using std:cout;
+using std:endl;
+
+int main()
+{
+   const int dafnaStart = 100;
+   const int lefnaStart = 100;
+   const double dafnaRaise = 0.10;
+   const double lefnaRaise = 0.05;
+   double dafnaAmount{};
+   double lefnaAmount{};
+
+   int 
+   while ( dafnaAmount != lefnaAmount);
+
+   return 0;
+}
