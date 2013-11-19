6b4698e7498445ad407f5646325757ea1af97780
Anton Melnyk <an.melnik@gmail.com>
2013-11-19 18:18:28 +0200

    task4 is done

--- chapter5/task4/task4.cpp
+++ chapter5/task4/task4.cpp
@@ -1,20 +1,28 @@
 #include <iostream>
 
-using std:cin;
-using std:cout;
-using std:endl;
+using std::cin;
+using std::cout;
+using std::endl;
 
-int main()
+int main(void)
 {
    const int dafnaStart = 100;
    const int lefnaStart = 100;
-   const double dafnaRaise = 0.10;
+   const double dafnaRaise = 0.1;
    const double lefnaRaise = 0.05;
-   double dafnaAmount{};
-   double lefnaAmount{};
+   const double dafnaProfit= dafnaStart * dafnaRaise; 
+   double dafnaAmount{ 100 };
+   double lefnaAmount{ 100 };
+   int years{};
 
-   int 
-   while ( dafnaAmount != lefnaAmount);
+   do
+   {
+      dafnaAmount += dafnaProfit;
+      lefnaAmount = lefnaAmount + (lefnaAmount * lefnaRaise);
+      years++;
+      cout << "Year: " << years << " Dafna: " << dafnaAmount << " Lefna: " << lefnaAmount << endl;
+   }
+   while ( dafnaAmount >= lefnaAmount);
 
    return 0;
 }
