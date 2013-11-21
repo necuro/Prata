7a8305a240aee11759d837a73bf8f18da54e4ebf
Anton Melnyk <an.melnik@gmail.com>
2013-11-21 20:41:30 +0200

    task5 is done

--- chapter5/task5/task5.cpp
+++ chapter5/task5/task5.cpp
@@ -8,6 +8,23 @@ using std:: string;
 
 int main(void)
 {
+   const int Months = 12;
+   string months[Months] { "January", "February", "March", "April"
+                   , "May", "June", "July", "August"
+                   , "September", "October", "November", "December"
+                   };
+
+   int sales[Months]{};
+   int sum{};
+
+   for( int i = 0; i < Months; i++ )
+   {
+      cout << "Input value for " << months[i] << ": ";
+      cin >> sales[i];
+      sum += sales[i];
+   }
+
+   cout << "Sum: " << sum << endl;;
 
    return 0;
 }
