92de8e3ee673f91842f41bee53a2a4c0ef5da10d
Anton Melnyk <an.melnik@gmail.com>
2013-11-21 21:37:02 +0200

    task6 is done

--- chapter5/task6/task6.cpp
+++ chapter5/task6/task6.cpp
@@ -9,14 +9,28 @@ using std:: string;
 int main(void)
 {
    const int Months = 12;
+   const int Years = 3;
    string months[Months] { "January", "February", "March", "April"
                    , "May", "June", "July", "August"
                    , "September", "October", "November", "December"
                    };
+   int sales[Years] [Months] {};
+   int sums[Years]{};
 
+   cout << "!!!Input sums of sales for 3 years!!!" << endl;
+   for( int i = 0; i < Years; i++ )
+   {
+      cout << "***Year number " << i + 1 << "***" << endl;
+      for( int j = 0; j < Months; j++ )
+      {
+         cout << months[j] << ": ";
+         cin >> sales[i] [j];
+         sums[i] += sales[i][j];
+      }
+      cout << "Sum of sales for year number " << i + 1 << " : " << sums[i] << endl;
    }
 
-   cout << "Sum: " << sum << endl;;
+   cout << "Sums of sales for 3 years: " << sums[0] + sums[1] + sums[2] << endl;
 
    return 0;
 }
