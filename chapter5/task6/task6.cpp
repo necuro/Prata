dd7e71f2e3d3cbc06a5ab749b4915670c8222da3
Anton Melnyk <an.melnik@gmail.com>
2013-11-21 21:17:34 +0200

    task6 in progress

---
+++ chapter5/task6/task6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std:: endl;
+using std:: string;
+
+int main(void)
+{
+   const int Months = 12;
+   string months[Months] { "January", "February", "March", "April"
+                   , "May", "June", "July", "August"
+                   , "September", "October", "November", "December"
+                   };
+
+   }
+
+   cout << "Sum: " << sum << endl;;
+
+   return 0;
+}
