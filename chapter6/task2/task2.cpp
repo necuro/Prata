e2cef72df3541464db6ad3a442cd7df43157963b
Anton Melnyk <an.melnik@gmail.com>
2013-11-27 23:04:12 +0200

    task2 is done

--- chapter6/task2/task2.cpp
+++ chapter6/task2/task2.cpp
@@ -9,21 +9,30 @@ int main(void)
 {
    const int SIZE = 10;
    double arr[SIZE];
+   int i = 0;
    
-   for( char tmp ,int i = 0; i < SIZE; i++ )
+   cout << "Arr [" << i << "]: ";
+   while( cin >> arr[i] && i < SIZE )
    {
-      cout << "Input value: ";
-      cin.get( tmp );
-      while( tmp )
-      {
-         if( isdigit( tmp ) )
-         {
-            cout << "It isn't number" << endl;
-            break;
-         }
-      }
-      
+      if(++i < SIZE)
+         cout << "Arr [" << i << "]: ";
    }
+
+   double average = 0;
+   for( int j = 0; j < i; j++ ) 
+   {
+      average += arr[j];
+   }
+   average /= i ;
+   cout << "Average: " << average << endl;
+
+   int counter = 0;
+   for( int j = 0; j < i; j++)
+   {
+      if(arr[j] > average)
+         counter++;
+   }   
+   cout << "Count of members bigger then avegare: " << counter << endl;
 
    return 0;
 }
