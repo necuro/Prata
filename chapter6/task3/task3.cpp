b4d652e69a50e619970df179db5323a21247aba8
Anton Melnyk <an.melnik@gmail.com>
2013-11-28 17:30:50 +0200

    task3 is done

--- chapter6/task3/task3.cpp
+++ chapter6/task3/task3.cpp
@@ -6,7 +6,37 @@ using std:: endl;
 
 int main(void)
 {
-      
+   char ch;
+   enum { c, p, t, g};
+
+   cout << "Please enter a c, p, t, or g: ";
+   cin >> ch;
+   while( ch != '0' )
+   {
+      switch( ch )
+      {
+         case 'c' : cout << "You have inputted: " << ch << endl;
+                    cout << "Please enter a c, p, t, or g: ";
+                    cin >> ch;
+                    break;
+         case 'p' : cout << "You have inputted: " << ch << endl;
+                    cout << "Please enter a c, p, t, or g: ";
+                    cin >> ch;
+                    break;
+         case 't' : cout << "You have inputted: " << ch << endl; 
+                    cout << "Please enter a c, p, t, or g: ";
+                    cin >> ch;
+                    break;
+         case 'g' : cout << "You have inputted: " << ch << endl;
+                    cout << "Please enter a c, p, t, or g: ";
+                    cin >> ch;
+                    break;
+         default:   cout << "You have inputted: " << ch << endl;
+                    cout << "Please enter a c, p, t, or g: "; 
+                    cin >> ch;
+                    break;
+      }
+   }
 
    return 0;
 }
