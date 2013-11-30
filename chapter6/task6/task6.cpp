1a29dce7333abb06c7a0ef21cbaa5bcbcfaf7812
Anton Melnyk <an.melnik@gmail.com>
2013-11-30 18:27:21 +0200

    task6 is done

---
+++ chapter6/task6/task6.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+struct Patron
+{
+   string Name;
+   double Sum;
+};
+
+int main(void)
+{
+   const int GRAND_LIMIT = 10000;
+   int SIZE;
+   cout << "Input number of patrons: ";
+   cin >> SIZE;
+
+//Declare an array of attributes for Grand/Not Grand patrons
+   bool grand[SIZE];
+
+//Declare an dynamic array of patrons
+   Patron* patrons = new Patron[SIZE];
+
+//Fill array of patrons
+   for( int i = 0; i < SIZE; i++)
+   {
+      cout << "Input info about patron:" << endl << "Name: ";
+      cin >> patrons[i].Name;
+      cout << "Sum: ";
+      cin >> patrons[i].Sum;
+//Fill array of flags for Grand/Not Grand patrons
+      if( patrons[i].Sum >= GRAND_LIMIT )
+         grand[i] = true;
+      else
+         grand[i] = false;
+   }
+   
+   cout << "Grand patrons:" << endl;
+//Flag if not some kind of patrons
+   bool flag;
+
+//Check and print grand patrons
+   for(int i = 0; i < SIZE; i++)
+   {
+      if( grand[i] )
+      {
+         cout << "Name: " << patrons[i].Name << ", Sum: " << patrons[i].Sum << endl;
+         flag = true;
+      }
+
+   }
+
+//If we don't have persons Grand patrons
+   if( !flag)
+      cout << "none\n";
+
+//Check and print not grand patrons
+   cout << "Patrons:" << endl;
+   flag = false;
+   for(int i = 0; i < SIZE; i++)
+   {
+      if( !grand[i] )
+      {
+         cout << "Name: " << patrons[i].Name << ", Sum: " << patrons[i].Sum << endl;
+         flag = true;
+      }
+   }
+
+//If we don't have patrons
+   if( !flag)
+      cout << "none\n";
+
+   
+
+   return 0;
+}
