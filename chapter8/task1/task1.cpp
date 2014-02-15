1e31e6fb714c0e1db2e3a4142afda66e3f3e55ad
Anton Melnyk <an.melnik@gmail.com>
2014-02-15 19:33:13 +0200

    task1 is done

--- chapter8/task1/task1.cpp
+++ chapter8/task1/task1.cpp
@@ -4,8 +4,41 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+int NumOfCalls = 0;
+
+void printString( const char*, int n = 0 );
+
+
 int main(void)
 {
+   char string[100];
+   int num;
 
+   cout << "Input number or q for quit: ";
+   while( cin >> num )
+   {
+      cout << "Enter string: ";
+      cin >> string;
+      printString( string, num );
+      cout << "Input number or q for quit: ";
+   }
+
+   cout << "Bye\n";
    return 0;
+}
+
+void printString( const char* pArrChar, int n )
+{
+   if( n != 0 )
+   {
+      for( int i = 0; i < NumOfCalls; i++ )
+         cout << pArrChar << endl;
+      cout << "String was printed " << NumOfCalls << " times\n";
+   }
+   else
+   {
+      cout << pArrChar << endl;
+   }
+   
+   ++NumOfCalls;
 }
